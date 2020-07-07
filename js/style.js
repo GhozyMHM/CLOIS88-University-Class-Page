749cfbfcd5dbcc23f5abbb0cae5efdd42d81efbb
GhozyMHM <gliozghozy@gmail.com>
2020-07-07 12:05:12 +0700

    Homepage Update

---
+++ js/style.js
@@ -0,0 +1,9 @@
+var navbar = document.getElementById('web-navbar');
+
+window.onscroll = function () {
+    if (window.pageYOffset > 80) {
+        navbar.style.background = "#F67280";
+    } else {
+        navbar.style.background = "transparent";
+    }
+}
